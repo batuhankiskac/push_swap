3d694903d4a07b7decbbad0d27db08c6ad8d2044
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-20 20:31:08 +0300

    Implement Makefile and add main function with header file for push_swap

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/01/20 20:00:51 by bkiskac           #+#    #+#              #
-#    Updated: 2025/01/20 20:01:30 by bkiskac          ###   ########.fr        #
+#    Updated: 2025/01/20 20:04:15 by bkiskac          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,4 +14,23 @@ NAME = push_swap
 CC = cc
 CFLAGS = -Wall -Wextra -Werror
 
-SRCS = 
+SRCS =
+
+OBJS = $(SRCS:.c=.o)
+
+all: $(NAME)
+	$(NAME): $(OBJS)
+	make -C ./libft
+	$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+clean:
+	rm -f $(OBJS)
+	make clean -C ./libft
+
+fclean: clean
+	rm -f $(NAME)
+	make fclean -C ./libft
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/20 20:15:03 by bkiskac           #+#    #+#             */
+/*   Updated: 2025/01/20 20:30:46 by bkiskac          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_stack
+{
+	long			value;
+	long			index;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}				t_stack;
+
+#endif

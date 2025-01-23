211ea4602c30c2ab6f5c889826213ed609ea1b90
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-23 22:33:41 +0300

    Refactor rotate_both and rev_rotate_both functions to use single cheapest node parameter

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 20:15:03 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/23 21:20:04 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/01/23 22:16:37 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,8 @@ void	current_index(t_stack *stack);
 void	ft_init_nodes_a(t_stack *stack_a, t_stack *stack_b);
 void	ft_init_nodes_b(t_stack *stack_a, t_stack *stack_b);
 void	ft_sort_stacks(t_stack **stack_a, t_stack **stack_b);
-void	rotate_both(t_stack **a, t_stack **b, t_stack **cheapest);
-void	rev_rotate_both(t_stack **a, t_stack **b, t_stack **cheapest);
+void	rotate_both(t_stack **a, t_stack **b, t_stack *cheapest);
+void	rev_rotate_both(t_stack **a, t_stack **b, t_stack *cheapest);
 void	move_a_to_b(t_stack **stack_a, t_stack **stack_b);
 void	move_b_to_a(t_stack **stack_a, t_stack **stack_b);
 void	min_on_top(t_stack **stack);
--- src/stack_utils_3.c
+++ src/stack_utils_3.c
@@ -6,30 +6,47 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 20:50:43 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/23 21:19:30 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/01/23 22:33:18 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	rotate_both(t_stack **a, t_stack **b, t_stack **cheapest)
+void	rotate_both(t_stack **a, t_stack **b, t_stack *cheapest)
 {
-
+	while (*a != cheapest && *b != cheapest->target_node)
+		rr(a, b, true);
+	current_index(*a);
+	current_index(*b);
 }
 
-void	rev_rotate_both(t_stack **a, t_stack **b, t_stack **cheapest)
+void	rev_rotate_both(t_stack **a, t_stack **b, t_stack *cheapest)
 {
-
+	while (*a != cheapest && *b != cheapest->target_node)
+		rrr(a, b, true);
+	current_index(*a);
+	current_index(*b);
 }
 
 void	move_a_to_b(t_stack **stack_a, t_stack **stack_b)
 {
+	t_stack	*cheapest_node;
 
+	cheapest_node = get_cheapest(*stack_a);
+	if (cheapest_node->above_median && cheapest_node->target_node->above_median)
+		rotate_both(stack_a, stack_b, cheapest_node);
+	else if (!(cheapest_node->above_median)
+		&& !(cheapest_node->target_node->above_median))
+		rev_rotate_both(stack_a, stack_b, cheapest_node);
+	prep_for_push(stack_a, cheapest_node, 'a');
+	prep_for_push(stack_b, cheapest_node->target_node, 'b');
+	pb(stack_b, stack_a, true);
 }
 
 void	move_b_to_a(t_stack **stack_a, t_stack **stack_b)
 {
-
+	prep_for_push(stack_a, (*stack_b)->target_node, 'a');
+	pa(stack_a, stack_b, true);
 }
 
 void	min_on_top(t_stack **stack)

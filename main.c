11f878de2fe2b25ea38cec14244d935d51ab5d8a
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-24 12:08:10 +0300

    Remove unused functions and main logic for stack sorting

--- main.c
+++ main.c
@@ -6,33 +6,9 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 20:14:39 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/23 22:42:06 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/01/24 12:07:55 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	main(int argc, char const *argv[])
-{
-	t_stack	*a;
-	t_stack	*b;
-
-	a = NULL;
-	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-		return (1);
-	else if (argc == 2)
-		argv = (const char **)ft_split(argv[1], ' ');
-	ft_stack_init(&a, argv + 1);
-	if (!ft_check_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, true);
-		else if (stack_len(a) == 3)
-			ft_sort_three(&a);
-		else
-			ft_sort_stacks(&a, &b);
-	}
-	free_stack(&a);
-	return (0);
-}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 20:15:03 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/24 00:01:11 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/01/24 12:08:02 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,39 +31,6 @@ typedef struct s_stack
 	struct s_stack	*prev;
 }				t_stack;
 
-t_stack	*find_last(t_stack *stack);
-t_stack	*find_max(t_stack *stack);
-t_stack	*find_min(t_stack *stack);
-t_stack	*get_cheapest(t_stack *stack);
-bool	ft_check_sorted(t_stack *stack);
-bool	error_duplicate(t_stack *stack, int n);
-bool	error_syntax(const char *str);
-void	ft_stack_init(t_stack **a, const char *argv[]);
-void	ft_sort_three(t_stack **a);
-void	sa(t_stack **stack_a, bool print);
-void	sb(t_stack **stack_b, bool print);
-void	ss(t_stack **stack_a, t_stack **stack_b, bool print);
-void	ra(t_stack **stack_a, bool print);
-void	rb(t_stack **stack_b, bool print);
-void	rr(t_stack **stack_a, t_stack **stack_b, bool print);
-void	rra(t_stack **stack_a, bool print);
-void	rrb(t_stack **stack_b, bool print);
-void	rrr(t_stack **stack_a, t_stack **stack_b, bool print);
-void	pa(t_stack **stack_a, t_stack **stack_b, bool print);
-void	pb(t_stack **stack_b, t_stack **stack_a, bool print);
-void	free_errors(t_stack **stack);
-void	free_stack(t_stack **stack);
-void	prep_for_push(t_stack **stack, t_stack *node, char name);
-void	current_index(t_stack *stack);
-void	ft_init_nodes_a(t_stack *stack_a, t_stack *stack_b);
-void	ft_init_nodes_b(t_stack *stack_a, t_stack *stack_b);
-void	ft_sort_stacks(t_stack **stack_a, t_stack **stack_b);
-void	rotate_both(t_stack **a, t_stack **b, t_stack *cheapest_node);
-void	rev_rotate_both(t_stack **a, t_stack **b, t_stack *cheapest_node);
-void	move_a_to_b(t_stack **a, t_stack **b);
-void	move_b_to_a(t_stack **a, t_stack **b);
-void	min_on_top(t_stack **a);
-void	min_on_top(t_stack **stack);
-int		stack_len(t_stack *stack);
+
 
 #endif

59bd63a3439e1bb08ee84f2346f1d71f1ea80668
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-24 12:11:36 +0300

    Add error handling functions and stack manipulation logic

---
+++ src/push.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 12:10:53 by bkiskac           #+#    #+#             */
+/*   Updated: 2025/01/24 12:11:23 by bkiskac          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+static void	push(t_stack **dst, t_stack **src)
+{
+	t_stack	*push_node;
+
+	if (!*src)
+		return ;
+	push_node = *src;
+	*src = (*src)->next;
+	if (*src)
+		(*src)->prev = NULL;
+	push_node->prev = NULL;
+	if (!*dst)
+	{
+		*dst = push_node;
+		push_node->next = NULL;
+	}
+	else
+	{
+		push_node->next = *dst;
+		push_node->next->prev = push_node;
+		*dst = push_node;
+	}
+}
+
+void	pa(t_stack **a, t_stack **b, bool print)
+{
+	push(a, b);
+	if (!print)
+		ft_printf("pa\n");
+}
+
+void	pb(t_stack **b, t_stack **a, bool print)
+{
+	push(b, a);
+	if (!print)
+		ft_printf("pb\n");
+}

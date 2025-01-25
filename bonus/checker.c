395e927db8d98962a95e2b5168ddea65045f8761
batuhankiskac <kiskacbatuhan@gmail.com>
2025-01-25 12:55:42 +0300

    Refactor command execution: update functions to use boolean flags for consistency

--- bonus/checker.c
+++ bonus/checker.c
@@ -6,7 +6,7 @@
 /*   By: bkiskac <bkiskac@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 20:38:31 by bkiskac           #+#    #+#             */
-/*   Updated: 2025/01/25 12:41:53 by bkiskac          ###   ########.fr       */
+/*   Updated: 2025/01/25 12:54:52 by bkiskac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,27 @@
 static int	read_operations(t_stack **stack_a, t_stack **stack_b, char *line)
 {
 	if (!ft_strncmp(line, "sa\n", 3))
-		sa(stack_a, false);
+		sa(stack_a, true);
 	else if (!ft_strncmp(line, "sb\n", 3))
-		sb(stack_b, false);
+		sb(stack_b, true);
 	else if (!ft_strncmp(line, "ss\n", 3))
-		ss(stack_a, stack_b, false);
+		ss(stack_a, stack_b, true);
 	else if (!ft_strncmp(line, "pa\n", 3))
-		pa(stack_a, stack_b, false);
+		pa(stack_a, stack_b, true);
 	else if (!ft_strncmp(line, "pb\n", 3))
-		pb(stack_b, stack_a, false);
+		pb(stack_b, stack_a, true);
 	else if (!ft_strncmp(line, "ra\n", 3))
-		ra(stack_a, false);
+		ra(stack_a, true);
 	else if (!ft_strncmp(line, "rb\n", 3))
-		rb(stack_b, false);
+		rb(stack_b, true);
 	else if (!ft_strncmp(line, "rr\n", 3))
-		rr(stack_a, stack_b, false);
+		rr(stack_a, stack_b, true);
 	else if (!ft_strncmp(line, "rra\n", 4))
-		rra(stack_a, false);
+		rra(stack_a, true);
 	else if (!ft_strncmp(line, "rrb\n", 4))
-		rrb(stack_b, false);
+		rrb(stack_b, true);
 	else if (!ft_strncmp(line, "rrr\n", 4))
-		rrr(stack_a, stack_b, false);
+		rrr(stack_a, stack_b, true);
 	else
 		return (0);
 	return (1);
@@ -53,15 +53,15 @@ static void	handle_error(t_stack **stack_a, t_stack **stack_b)
 
 int	main(int argc, char **argv)
 {
-	t_stack *stack_a;
-	t_stack *stack_b;
-	char    *line;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	char	*line;
 
 	if (argc < 2)
 		return (0);
-
 	stack_b = NULL;
-	stack_a = ft_stack_init(argc, argv);
+	stack_a = NULL;
+	init_stack(&stack_a, argv);
 	while (1)
 	{
 		line = get_next_line(0);
@@ -74,7 +74,7 @@ int	main(int argc, char **argv)
 		}
 		free(line);
 	}
-	if (ft_check_sorted(stack_a) && !stack_b)
+	if (stack_sorted(stack_a) && !stack_b)
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
